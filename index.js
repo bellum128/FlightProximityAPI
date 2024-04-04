b96c0bebe547644d4b3fc090af1bae4667cd0653
bellum128 <bellum128@gmail.com>
2024-04-03 21:54:25 -0400

    Code cleanup.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import {fetchFromRadar} from 'flightradar24-client'
+import { fetchFromRadar } from 'flightradar24-client'
 import express from 'express';
 const app = express();
 const port = 6129;
 
-
 app.get("/getflights", (req, res) => {
-
     let expand_radius = ((req.query.expand_radius) ? parseFloat(req.query.expand_radius) : 0);
 
     let querySet = {
@@ -21,37 +19,37 @@ app.get("/getflights", (req, res) => {
     console.log("Query Set:");
     console.log(querySet);
 
-    
-    let fin = {};
+    let final = {};
     fetchFromRadar(querySet.north_lat, querySet.west_long, querySet.south_lat, querySet.east_long)
-    .then((flights) => {
+        .then((flights) => {
 
-        flights.forEach(curr => {
-            // Calculate approx. distance in miles
-            curr.distanceToCenter = (Math.sqrt( Math.pow((curr.latitude - querySet.center_lat),2) + Math.pow((curr.longitude - querySet.center_long),2)) * 69);
+            flights.forEach(curr => {
+                // Calculate approx. distance in miles
+                curr.distanceToCenter = (Math.sqrt(Math.pow((curr.latitude - querySet.center_lat), 2) + Math.pow((curr.longitude - querySet.center_long), 2)) * 69);
+            });
+
+
+            flights.sort((a, b) => (a.distanceToCenter > b.distanceToCenter) ? 1 : -1);
+            console.log(flights);
+
+            final = {
+                flights: flights,
+                success: true
+            }
+
+            res.send(final);
+        })
+        .catch((e) => {
+            console.log(e);
+
+            final = {
+                success: false
+            };
+
+            res.send(final);
         });
-
-
-        flights.sort((a, b) => (a.distanceToCenter > b.distanceToCenter) ? 1 : -1);
-        console.log(flights);
-
-        fin = {
-            flights: flights,
-            success: true
-        }
-
-        res.send(fin);
-    })
-    .catch((e) => {
-        console.log(e);
-        fin = {
-            success: false
-        };
-
-        res.send(fin);
-    });
 });
 
 app.listen(port, () => {
-    console.log(`Flight Display API listening at http://localhost:${port}`);
+    console.log(`Flight Display API listening on port ${port}`);
 });
